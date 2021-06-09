d4d2a0bb0981c3d4d50823651b1fabce9cd00d23
nishprojects <nish@iwscorp.com>
2021-06-09 10:01:49 -0600

    changes

--- iws_tools/heartbeat.py
+++ iws_tools/heartbeat.py
@@ -7,10 +7,12 @@ from pathlib import Path
 import inspect
 
 class Heartbeat:
-    def __init__(self, server_ip=None, server_port=None, pub_callback=None, hourOffset=None):
+    def __init__(self, server_ip=None, server_port=None, host_name=None, hasSettings=False, pub_callback=None, hourOffset=None):
         self.server_ip = server_ip
         self.server_port = server_port
         self.pub_callback = pub_callback
+        self.host_name = host_name
+        self.hasSettings = hasSettings
         self.rc = 0
         if self.pub_callback is None:
             self.rc = 1
@@ -39,7 +41,6 @@ class Heartbeat:
         
         while self.event.wait(1) is False:
             heartbeat = {}
-            host_name = socket.gethostname()
             address = (self.server_ip, self.server_port)
             if (self.server_ip is not None) & (self.server_port is not None):
                 ip_addr = None
@@ -47,9 +48,10 @@ class Heartbeat:
                     with socket.create_connection(address=address, timeout=1) as con:
                         ip_addr = con.getsockname()[0]
                 except:
-                    ip_addr = socket.gethostbyname(host_name)
-                heartbeat.update({'hostName': host_name,
-                                'ipAddress': ip_addr,})
+                    ip_addr = socket.gethostbyname(self.host_name)
+                heartbeat.update({'hostName': self.host_name,
+                                'ipAddress': ip_addr,
+                                  'hasSettings':self.hasSettings})
             if self.hourOffset is not None:
                 localTime = datetime.utcnow()+ timedelta(hours=self.hourOffset)
                 last_heartbeat = localTime.isoformat(sep=' ', timespec='milliseconds')
@@ -66,13 +68,4 @@ class Heartbeat:
             thread = Thread(target=self.heartbeat_thread, name='Heartbeat Thread')
             thread.start()
         return self.rc
-    #tseting
-    def generate_person():
-        print(
-            {
-                "name": "Samuel Mendez",
-                "email": "michelle71@hotmail.com",
-                "password": "*hMRsQBbK1",
-                "address": "78287 Morgan Summit\nPhillipsstad, WV 38051"
-            }
-        )    
+    
